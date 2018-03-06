220181423b25e4f7c9bf6d647fbcd210818649ea
Dominique Meeks <dominique.l.meeks@gmail.com>
2018-03-05 21:02:42 -0500

    progress

--- app.js
+++ app.js
@@ -1,37 +1,79 @@
 $(document).ready(function(){
- var getUrl = function()
-    {
-        url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-        url += '?' + $.param({
-        'api-key': "c55851da8c4a4546a1d3541feb1cdd46",
-        'q': "movies",
-        'begin_date': "20170101",
-        'end_date': "20180101"
-        });
-        return url
-    }
- $.ajax({
-   url: getUrl(),
-   method: 'GET',
- }).done(function(result) {
-    var docs = result.response.docs,
-        thisDoc,
-        title,
-        author,
-        pubDate,
-        url;
 
-    for (var i =0; i < docs.length; i++)
-    {
-        thisDoc = docs[i];
-        title = thisDoc.headline.main;
-        author = thisDoc.byline.original;
-        pubDate = thisDoc.pubDate;
-        url = thisDoc.web_url;
+var obj = {
+        qStr: "",
+        count: 0,
+        fields: {
+            searchTerm: '',
+            numberRecords:'',
+            startYear:'',
+            endYear:''
+        },
+        parms: {
+            'api-key': "c55851da8c4a4546a1d3541feb1cdd46",
+            'q': "",
+            'begin_date': "",
+            'end_date': ""
+        },
+        url: "",
+        getUrl: function()
+        {
+            this.url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+
+            //get string for all param
+            $.each(this.params, function(key, value) {
+                if (value != "")
+                {
+                    this.url += key + '=' + value;
+                }
+            })
+            
+        },
+        getAJAX: function()
+        {
+            $.ajax({
+                url: this.url,
+                method: 'GET',
+            }).then(function(result) {
+                var docs = result.response.docs,
+                    thisDoc = {                     
+                        title:"",
+                        author:"",
+                        pubDate:"",
+                        url:"",
+                    },
+                    
+                // loop through all docs
+                for (var i =0; i < docs.length; i++)
+                {
+                    title = docs[i].headline.main;
+                    author = docs[i].byline.original;
+                    pubDate = docs[i].pubDate;
+                    url = docs[i].web_url;
+                }
+            })
+        },
+        getArticleHTML: function(docObj)
+        {
+            $('#qSum').html(`<div class="row">
+                                    <div class = "left">
+                                        <b>Question:</b> ${this.qIndx + 1} of ${this.qBank.length} 
+                                    </div>
+                                    <div class = "center">
+                                        <b>Category:</b> ${qObj.category}
+                                    </div>
+                                    <div class = "right">
+                                        <b>Difficulty:</b> ${qObj.difficulty} 
+                                    </div>                                    
+                                </div>`);
+        }
     }
- }).fail(function(err) {
-   throw err;
- });
+
+    $('#search').on('click', function(){
+        $.each(obj.fields, function(key, value) {
+            this.fields[key] = $('#' + key).val();
+        })
+    })
 
 });
 
