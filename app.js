2780a60cbfce7916f64f757110549b7e62e3d7a4
Dominique Meeks <dominique.l.meeks@gmail.com>
2018-03-05 22:10:50 -0500

    It's Working!

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ var obj = {
             startYear:'',
             endYear:''
         },
-        parms: {
-            'api-key': "c55851da8c4a4546a1d3541feb1cdd46",
+        parms: {            
             'q': "",
             'begin_date': "",
             'end_date': ""
@@ -18,18 +17,19 @@ var obj = {
         url: "",
         getUrl: function()
         {
-            this.url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
-
+            this.url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?" +
+                        "api-key=c55851da8c4a4546a1d3541feb1cdd46";
+            
             //get string for all param
             $.each(this.params, function(key, value) {
                 if (value != "")
                 {
-                    this.url += key + '=' + value;
+                    this.url += '&' + key + '=' + value;
                 }
             })
             
         },
-        getAJAX: function()
+        getAJAX: function(url)
         {
             $.ajax({
                 url: this.url,
@@ -41,38 +41,62 @@ var obj = {
                         author:"",
                         pubDate:"",
                         url:"",
-                    },
+                    };
                     
                 // loop through all docs
-                for (var i =0; i < docs.length; i++)
+                for (var i =0; i < obj.fields.numberRecords; i++)
                 {
-                    title = docs[i].headline.main;
-                    author = docs[i].byline.original;
-                    pubDate = docs[i].pubDate;
-                    url = docs[i].web_url;
+                    thisDoc.title = docs[i].headline.main;
+                    thisDoc.author = docs[i].byline.original;
+                    thisDoc.pubDate = docs[i].pub_date;
+                    thisDoc.url = docs[i].web_url;
+                    obj.getArticleHTML(thisDoc);
                 }
             })
         },
         getArticleHTML: function(docObj)
         {
-            $('#qSum').html(`<div class="row">
-                                    <div class = "left">
-                                        <b>Question:</b> ${this.qIndx + 1} of ${this.qBank.length} 
-                                    </div>
-                                    <div class = "center">
-                                        <b>Category:</b> ${qObj.category}
-                                    </div>
-                                    <div class = "right">
-                                        <b>Difficulty:</b> ${qObj.difficulty} 
-                                    </div>                                    
-                                </div>`);
+            $('#articleHeader').append(`<div class="row articleRow">
+                                            <div class="title">${docObj.title}</div>
+                                            <div class="content subSect">
+                                                <div class="author">${docObj.author}</div>
+                                                <div class="date">${docObj.pubDate}</div>
+                                                <a target="blank" href="${docObj.url}" 
+                                                    class ="url">${docObj.url}</a>
+                                            </div>
+                                        </div>`);
+        },
+        getFormData: function()
+        {
+            var self = this;
+            return new Promise(
+                function(resolve, reject) {
+                    try 
+                    {
+                        $.each(obj.fields, function(key, value) {
+                            self.fields[key] = $('#' + key).val();
+                        }); 
+
+                        self.parms['q'] = (self.fields['searchTerm']!='') ? self.fields['searchTerm'] : '';
+                        self.parms['begin_date'] = (self.fields['startYear']!='') ? (self.fields['startYear'] + '0101') : '';
+                        self.parms['end_date'] = (self.fields['endYear'] !='') ? (self.fields['endYear'] + '1231') : '';
+                        
+                        resolve(true);
+                    }
+                    catch(e)
+                    {
+                        reject(false);
+                    }                        
+                });           
+            
         }
     }
 
-    $('#search').on('click', function(){
-        $.each(obj.fields, function(key, value) {
-            this.fields[key] = $('#' + key).val();
-        })
+    $('#search').on('click', function(e){
+        event.preventDefault();
+        obj.getFormData().then(function(e) {
+            obj.getAJAX(obj.getUrl());
+        });           
     })
 
 });
